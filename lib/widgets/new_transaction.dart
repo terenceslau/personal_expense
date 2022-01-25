6894953c0a6b537e6f7ecb2e1c98ea0237c5b756
terenceslau <terence.sl.au@icloud.com>
2022-01-25 12:52:04 +0800

    Refactoring: widget breakdown, add builder function

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -4,6 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import './adaptive_flat_button.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addTx;
 
@@ -92,27 +94,10 @@ class _NewTransactionState extends State<NewTransaction> {
                           : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                     ),
                   ),
-                  Platform.isIOS
-                      ? CupertinoButton(
-                          color: Colors.blue,
-                          child: Text(
-                            'Choose Date',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onPressed: _presentDatePicker,
-                        )
-                      : FlatButton(
-                          textColor: Theme.of(context).primaryColor,
-                          child: Text(
-                            'Choose Date',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onPressed: _presentDatePicker,
-                        ),
+                  AdaptiveFlatButton(
+                    text: 'Choose date',
+                    handler: _presentDatePicker,
+                  ),
                 ]),
               ),
               Row(
